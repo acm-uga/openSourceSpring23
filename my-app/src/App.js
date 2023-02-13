5047610cc33beb15fc0accd993ba7cf0ca02e818
Ashwinkumar Pillai <ashwin@s172-20-185-h164.paws.uga.edu>
2023-02-13 15:45:26 -0500

    Implemented Routing. Added login Route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
-import Navbar from './components/Navbar/Navbar';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+// import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Login from "./components/Login/login";
 
 function App() {
   return (
-    <Navbar />
-  )
+    <React.Fragment>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
+
   /*
   return (
     <div className="App">
