d77ccba8cf29806cfa0b179ce0c35dd3fc19e6cd
cpitser <44904913+cpitser@users.noreply.github.com>
2023-02-16 13:55:52 -0500

    Post grid implemented

--- my-app/src/components/HousingPage/HousingPage.jsx
+++ my-app/src/components/HousingPage/HousingPage.jsx
@@ -6,47 +6,79 @@ import "./HousingPage.css";
 function HousingPage() {
   return (
     <div className="main-container">
-      <div className="post-display">
-        Hello
+
+      <div className="posts-container">
+        <div className="post post-1">
+          <table>
+            <tr>Housing 1</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+          
+        <div className="post post-2">
+          <table>
+            <tr>Housing 2</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+          
+        <div className="post post-1">
+          <table>
+            <tr>Housing 3</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+        
+        <div className="post post-2">
+          <table>
+            <tr>Housing 4</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+          
+        <div className="post post-1">
+          <table>
+            <tr>Housing 5</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+
+        <div className="post post-1">
+          <table>
+            <tr>Housing 6</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+          
+        <div className="post post-2">
+          <table>
+            <tr>Housing 7</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+          
+        <div className="post post-1">
+          <table>
+            <tr>Housing 8</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+          
+        <div className="post post-2">
+          <table>
+            <tr>Housing 9</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
+          
+        <div className="post post-1">
+          <table>
+            <tr>Housing 10</tr>
+            <tr><button type="submit">View Post</button></tr>
+          </table>
+        </div>
       </div>
 
-      <table className="container-of-posts">
-        <tr>
-          <div className="post-container post-container-1">
-          <table>
-              <tr>Housing 1</tr>
-              <tr><button type="submit">View Post</button></tr>
-            </table>
-          </div>
-        </tr>
-        
-        <tr>
-          <div className="post-container post-container-2">
-          <table>
-              <tr>Housing 2</tr>
-              <tr><button type="submit">View Post</button></tr>
-            </table>
-          </div>
-        </tr>
-
-        <tr>
-          <div className="post-container post-container-1">
-            <table>
-              <tr>Housing 3</tr>
-              <tr><button type="submit">View Post</button></tr>
-            </table>
-          </div>
-        </tr>
-
-        <tr>
-          <div className="post-container post-container-2">
-            <table>
-              <tr>Housing 4</tr>
-              <tr><button type="submit">View Post</button></tr>
-            </table>
-          </div>
-        </tr>
-      </table>
     </div>
   );
 }
