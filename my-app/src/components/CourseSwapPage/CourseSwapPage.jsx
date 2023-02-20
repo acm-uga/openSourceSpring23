1e6b47dcceeefbd88b2a8947739eb4aaa7153aa9
Sha Hos <107161530+ShaHos348@users.noreply.github.com>
2023-02-20 13:00:49 -0500

    Modified the search tab on courseSwap page

--- my-app/src/components/CourseSwapPage/CourseSwapPage.jsx
+++ my-app/src/components/CourseSwapPage/CourseSwapPage.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from '../../assets/book-swap.png';
+import search from '../../assets/search.png';
 
 import "./CourseSwapPage.css";
 
 function CourseSwapPage() {
   return (
-    <div className="main-container">
+    <div className="swap-container">
 
       <div className="side-tab">
-        <img id="logo" src={logo} alt="course-swap-logo"></img>
-        <Link>Find</Link>
-        <Link>Offer</Link>
-        <Link>Schedule</Link>
-        <Link>Saved</Link>
+        <img src={logo} alt="course-swap-logo" width={"50%"}></img>
+        <div className="side-list">
+          <Link>Find</Link>
+          <Link>Offer</Link>
+          <Link>Schedule</Link>
+          <Link>Saved</Link>
+        </div>
+        
       </div>
 
       <div className="search">
-        <input type="text" placeholder="Search"></input>
+        <input type="text" placeholder="Search" id="search-input"></input>
+        <img id="search-img" src={search} alt="search" width={"30vh"}></img>
+        <button id="sort">SORT</button>
+        <button id="filter">FILTER</button>
       </div>
 
     </div>
