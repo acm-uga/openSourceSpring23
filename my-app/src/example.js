a1ab06cb723f474f2542a1dd9db4c65240fe4232
jdhubb99 <jacobdhubbard@gmail.com>
2023-02-21 01:49:43 -0500

    style: Add pre-commit hook for formatting with prettier

--- my-app/src/example.js
+++ my-app/src/example.js
@@ -1,14 +1,12 @@
-import React from "react";
-import "./MyComponent.css";
+import React from 'react';
+import './example.css';
 
-
-function MyComponent({ title, text, onClick, buttonLabel }}) {
+function MyComponent({ title, text, onClick, buttonLabel }) {
   return (
     <div className="my-component">
       <h1 className="my-component__title">{title}</h1>
       <p className="my-component__text">{text}</p>
-      <button type='button'className="my-component__button"
-        onClick={onClick}>
+      <button type="button" className="my-component__button" onClick={onClick}>
         {buttonLabel}
       </button>
     </div>
