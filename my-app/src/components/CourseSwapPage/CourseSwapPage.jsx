df4070a1fe687510c615be1318be71215b12b73b
Sha Hos <107161530+ShaHos348@users.noreply.github.com>
2023-03-01 13:58:33 -0500

    Made some progress with course page and css

--- my-app/src/components/CourseSwapPage/CourseSwapPage.jsx
+++ my-app/src/components/CourseSwapPage/CourseSwapPage.jsx
@@ -11,6 +11,7 @@ import '../MainTheme.css';
 function CourseSwapPage() {
   return (
     <div className="swap-container">
+      {/*div for sidebar with pages in CourseSwap*/}
       <div className="side-tab">
         <img src={logo} alt="course-swap-logo" width={'50%'} />
         <div className="side-list">
@@ -22,9 +23,10 @@ function CourseSwapPage() {
         </div>
       </div>
 
+      {/*div for search tab*/}
       <div className="search">
         <input type="text" placeholder="Search" id="search-input" />
-        <img id="search-img" src={search} alt="search" width="30vh" />
+        <img id="search-img" src={search} alt="search" width="20vw" height="100%"/>
         <button type="button" id="sort">
           SORT
         </button>
@@ -32,6 +34,24 @@ function CourseSwapPage() {
           FILTER
         </button>
       </div>
+
+      {/*div for main course swap content*/}
+      <div className='content'>
+        <div>
+          <div id='user'>ShaHos348</div>
+          <div id='tabs'>
+            <button class="tablinks" onclick="open(event, 'Offer')">Offered</button>
+            <button class="tablinks" onclick="open(event, 'Request')">Requested</button>
+          </div>
+        </div>
+        <div id='name'>POLS 1101 - American Government</div>
+        <div id='instructor'>Shahbin Hossain</div>
+        <div id='crn'>42069</div>
+        <div id='time'>[------]</div>
+        <div id='location'>Park Hall</div>
+        <div id='option'>DEAL</div>
+      </div>
+      
     </div>
   );
 }
