fd06915fe10499b565ceb10c10d21948fa79499c
Sha Hos <107161530+ShaHos348@users.noreply.github.com>
2023-03-07 23:50:48 -0500

    Seperated the search bar and added some content to sort and filter tabs

--- my-app/src/components/CourseSwapPage/CourseSwapPage.jsx
+++ my-app/src/components/CourseSwapPage/CourseSwapPage.jsx
@@ -10,6 +10,51 @@ import '../MainTheme.css';
 // disable eslint for Links right now
 /* eslint-disable */
 function CourseSwapPage() {
+  return (
+    <div className="swap-container">
+      {/*div for sidebar with pages in CourseSwap*/}
+      <SideBar />
+
+      {/*div for search tab*/}
+      <SearchBar />
+
+      {/*div for main course swap content*/}
+      <div className="content">
+        <div>
+          <div id="user">ShaHos348</div>
+          <div id="gap"></div>
+          <div id="tabs">
+            <button className="tab-links">Offered</button>
+            <button className="tab-links">Requested</button>
+          </div>
+        </div>
+        <div id="name">POLS 1101 - American Government</div>
+        <div id="instructor">Shahbin Hossain</div>
+        <div id="crn">42069</div>
+        <div id="time">[------]</div>
+        <div id="location">Park Hall</div>
+        <button id="option">DEAL</button>
+      </div>
+    </div>
+  );
+}
+
+function SideBar() {
+  return (
+    <div className="side-tab">
+      <img src={logo} alt="course-swap-logo" width={'50%'} />
+      <div className="side-list">
+        {/* TODO: fix links/buttons */}
+        <Link className="text-link">Find</Link>
+        <Link className="text-link">Offer</Link>
+        <Link className="text-link">Schedule</Link>
+        <Link className="text-link">Saved</Link>
+      </div>
+    </div>
+  );
+}
+
+function SearchBar() {
   const [courseFilterTab, setCourses] = useState('false');
   const [courseSortTab, setSort] = useState('false');
   const toggleCourses = () => {
@@ -30,55 +75,45 @@ function CourseSwapPage() {
   };
 
   return (
-    <div className="swap-container">
+    <div className="search">
       <div className={courseFilterTab ? 'hideC popupC' : 'showC popupC'}>
-        Course filters
+        Course Filters
+        <div>
+          <div>
+            <label>Subject</label>
+            <input type="text" placeholder="Subject" />
+          </div>
+          <div>
+            <label>Course Number</label>
+            <input type="text" placeholder="Course Number" />
+          </div>
+          <div>
+            <label>Location</label>
+            <input type="text" placeholder="Location" />
+          </div>
+          <div>
+            <label>Instructor</label>
+            <input type="text" placeholder="Instructor" />
+          </div>
+        </div>
       </div>
       <div className={courseSortTab ? 'hideC popupC' : 'showC popupC'}>
         Course Sorts
-      </div>
-
-      {/*div for sidebar with pages in CourseSwap*/}
-      <div className="side-tab">
-        <img src={logo} alt="course-swap-logo" width={'50%'} />
-        <div className="side-list">
-          {/* TODO: fix links/buttons */}
-          <Link className="text-link">Find</Link>
-          <Link className="text-link">Offer</Link>
-          <Link className="text-link">Schedule</Link>
-          <Link className="text-link">Saved</Link>
-        </div>
-      </div>
-
-      {/*div for search tab*/}
-      <div className="search">
-        <input type="text" placeholder="Search" id="search-input" />
-        <img id="search-img" src={search} alt="search" />
-        <button type="button" id="sort" onClick={toggleSort}>
-          SORT
-        </button>
-        <button type="button" id="filter" onClick={toggleCourses}>
-          FILTER
-        </button>
-      </div>
-
-      {/*div for main course swap content*/}
-      <div className="content">
         <div>
-          <div id="user">ShaHos348</div>
-          <div id="gap"></div>
-          <div id="tabs">
-            <button className="tab-links">Offered</button>
-            <button className="tab-links">Requested</button>
-          </div>
+          <button>Title</button>
+          <button>Subject</button>
+          <button>Course Number</button>
+          <button>CRN</button>
         </div>
-        <div id="name">POLS 1101 - American Government</div>
-        <div id="instructor">Shahbin Hossain</div>
-        <div id="crn">42069</div>
-        <div id="time">[------]</div>
-        <div id="location">Park Hall</div>
-        <button id="option">DEAL</button>
       </div>
+      <input type="text" placeholder="Search" id="search-input" />
+      <img id="search-img" src={search} alt="search" />
+      <button type="button" id="sort" onClick={toggleSort}>
+        SORT
+      </button>
+      <button type="button" id="filter" onClick={toggleCourses}>
+        FILTER
+      </button>
     </div>
   );
 }
