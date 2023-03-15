59f258956e2dc3f314eb9a7e518d0f8e2f30de60
stephennemeth <stephennemeth4@gmail.com>
2023-03-14 22:27:54 -0400

    Users can authenticate

---
+++ my-app/src/firebase.js
@@ -0,0 +1,73 @@
+import { initializeApp } from 'firebase/app';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from 'firebase/auth';
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from 'firebase/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD0wnaD8SQZw9h2D0nJ0XRjUHS4nAxtf8s',
+  authDomain: 'uga-marketplace.firebaseapp.com',
+  projectId: 'uga-marketplace',
+  storageBucket: 'uga-marketplace.appspot.com',
+  messagingSenderId: '229277322852',
+  appId: '1:229277322852:web:5dc4f06b41a379da08d971',
+  measurementId: 'G-SKKXFXRG35',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  console.log(email);
+  console.log(password);
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert('Success');
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  logout,
+};
