0617aa883fd090bf8eed2237d24fe74953559c3e
stephennemeth <stephennemeth4@gmail.com>
2023-04-03 13:20:37 -0400

    Basic Framework for the ticket frontend

--- my-app/src/components/TicketPage/TicketPage.jsx
+++ my-app/src/components/TicketPage/TicketPage.jsx
@@ -2,7 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../MainTheme.css';
 
+import Ticket from './Ticket';
+
 const TicketPage = () => {
+  const [tickets, setTickets] = useState([]);
+
+  const getTickets = async () => {
+    const response = await fetch(
+      'http://localhost:8080/api/tickets/getTicketsAll'
+    );
+    const json = await response.json();
+    setTickets(json);
+  };
+
+  useEffect(() => {
+    getTickets();
+  }, []);
+
+  if (tickets.length === 0) {
+    return <div>Loading Tickets...</div>;
+  }
+
   return (
     <div className="side-tab">
       <div className="side-list">
@@ -12,7 +32,13 @@ const TicketPage = () => {
         <Link className="text-link">Home/Away</Link>
       </div>
 
-      <div className=""></div>
+      <div className="ticket-list">
+        {tickets.map((ticket, index) => {
+          return <Ticket {...ticket} key={index} />;
+        })}
+      </div>
     </div>
   );
 };
+
+export default TicketPage;
