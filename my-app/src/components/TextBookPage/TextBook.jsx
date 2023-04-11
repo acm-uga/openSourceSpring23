7799f22cf0ce18b5ebb6c76423d2f44be09aad10
alexteal <ateal3235@gmail.com>
2023-04-11 19:10:32 -0400

    BookShelf component handles multiple BookBlocks

--- my-app/src/components/TextBookPage/TextBook.jsx
+++ my-app/src/components/TextBookPage/TextBook.jsx
@@ -4,6 +4,45 @@ import './TextBook.css';
 import '../MainTheme.css';
 import BookBlock from './BookBlock.jsx';
 
+const Bookshelf = () => {
+  const books = [
+    {
+      author: 'Alex',
+      title: 'The Art of Programming',
+      seller: 'Books R Us',
+      price: 29.99,
+      condition: 'New',
+      imageURL: 'https://m.media-amazon.com/images/I/61s6zH2bbIL._SX260_.jpg',
+    },
+    {
+      author: 'Yushus',
+      title: 'Data Science for Beginners',
+      seller: 'Bookworms',
+      price: 42.0,
+      condition: 'Used',
+      imageURL: 'https://m.media-amazon.com/images/I/61s6zH2bbIL._SX260_.jpg',
+    },
+    {
+      author: 'Gee Pea Tee',
+      title: 'Algorithms and Data Structures in Python',
+      seller: 'Techie Books',
+      price: 65.0,
+      condition: 'New',
+      imageURL: 'https://m.media-amazon.com/images/I/61s6zH2bbIL._SX260_.jpg',
+    },
+  ];
+  return (
+    <div
+      style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+      {books.map((book, index) => (
+        <div key={index} style={{ padding: '10px' }}>
+          <BookBlock {...book} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
 function TextBookPage() {
   return (
     <div className="textbook-container">
@@ -29,34 +68,7 @@ function TextBookPage() {
           </select>
         </div>
       </div>
-
-      <div className="row px-4">
-        <div className="col-sm-3">
-          <BookBlock
-            author={'Bob Bobert'}
-            title={'Data Structures & Algos in Java'}
-            seller={'Yushus Komarlu'}
-            price={57.0}
-            condition={'New'}
-            imageURL={
-              'https://m.media-amazon.com/images/I/61s6zH2bbIL._SX260_.jpg'
-            }
-          />
-        </div>
-
-        <div className="col-sm-3">
-          <BookBlock
-            author={'Bob Bobert'}
-            title={'Data Structures & Algos in Java'}
-            seller={'Yushus Komarlu'}
-            price={57.0}
-            condition={'New'}
-            imageURL={
-              'https://m.media-amazon.com/images/I/61s6zH2bbIL._SX260_.jpg'
-            }
-          />
-        </div>
-      </div>
+      <Bookshelf></Bookshelf>
     </div>
   );
 }
