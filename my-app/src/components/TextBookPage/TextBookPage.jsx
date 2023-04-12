f2b463ad6d894bdc1c30e835556eaba8326cfeab
ykomarlu <30438247+ykomarlu@users.noreply.github.com>
2023-04-12 17:19:49 -0400

    stash

--- my-app/src/components/TextBookPage/TextBookPage.jsx
+++ my-app/src/components/TextBookPage/TextBookPage.jsx
@@ -5,62 +5,23 @@ import { Link, Route, Router } from 'react-router-dom';
 import './TextBook.css';
 import '../MainTheme.css';
 import Profile from '../ProfilePage/Profile';
-
-function BookBlock({ author, title, seller, price, condition, imageURL }) {
-  return (
-    <div className="bookblock" style={{ width: 12 + 'rem' }}>
-      <img
-        className="card-img-top"
-        height={200}
-        src={imageURL}
-        alt="Card image cap"></img>
-      <div className="card-body">
-        <h6 className="card-title">Title: {title}</h6>
-        <h6 className="card-subtitle">Author: {author}</h6>
-        <br />
-        <div className="col">
-          {/* <Router> 
-    <Routes>
-    <Route path="/Profile" element={<Profile />}> 
-      <img className="profile-pic"
-          src="https://www.seekpng.com/png/full/428-4287240_no-avatar-user-circle-icon-png.png"
-          width="100"
-          height="100" />
-    </Route>
-    </Routes>
-    </Router> */}
-          <img
-            className="profile-pic"
-            src="https://www.seekpng.com/png/full/428-4287240_no-avatar-user-circle-icon-png.png"
-            width="100"
-            height="100"></img>
-        </div>
-        <div>
-          <p className="card-text">Username: {seller}</p>
-        </div>
-        <p className="card-text">Price: ${price}</p> <br />
-        <p className="card-text">Condition: {condition}</p>
-        <a href="#" className="btn btn-primary">
-          View
-        </a>
-      </div>
-    </div>
-  );
-}
+import { useEffect, useState } from 'react';
 
 /* eslint-disable */
 function TextBookPage() {
+  // Set the contentes of the profileTab
+
+  //set onclick function so that it changes the show value from false to true and vice versa
+
+  //have something to make the page show vs not show when the state value for the profiletab is true vs false
+
   return (
     <div className="textbook-container">
       <div className="side-tab">
         <div className="side-list">
           {/* TODO: fix links/buttons */}
-          <button className="button" onClick={TogglePrice}>
-            Price
-          </button>
-          <button className="button" onClick={ToggleCondition}>
-            Condition
-          </button>
+          <button className="button">Price</button>
+          <button className="button">Condition</button>
         </div>
       </div>
 
@@ -117,6 +78,81 @@ function TextBookPage() {
             imageURL={
               'https://m.media-amazon.com/images/I/61s6zH2bbIL._SX260_.jpg'
             }></BookBlock>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function BookBlock({ author, title, seller, price, condition, imageURL }) {
+  const [profileTab, setProfile] = useState('false');
+
+  const ToggleAll = () => {
+    if (!profileTab) {
+      setProfile(!profileTab);
+    }
+  };
+
+  const ToggleProfile = () => {
+    ToggleAll();
+    setProfile(!profileTab);
+  };
+
+  return (
+    <div className="bookblock" style={{ width: 12 + 'rem' }}>
+      <img
+        className="card-img-top"
+        height={200}
+        src={imageURL}
+        alt="Card image cap"></img>
+      <div className="card-body">
+        <h6 className="card-title">Title: {title}</h6>
+        <h6 className="card-subtitle">Author: {author}</h6>
+        <br />
+        <div className="col">
+          <img
+            className="profile-pic"
+            src="https://www.seekpng.com/png/full/428-4287240_no-avatar-user-circle-icon-png.png"
+            onClick={ToggleProfile}
+            width="100"
+            height="100"></img>
+          <div className={profileTab ? 'hide popup' : 'show popup'}>
+            <h3>Yushus Komarlu</h3>
+            <table className="activityTable">
+              <tr>
+                <th>Date</th>
+                <th>Account Name</th>
+                <th>User Rating</th>
+                <th>Description</th>
+              </tr>
+              <tr>
+                <td>09/14/2022</td>
+                <td>Daniel Grigsby</td>
+                <td>5 Stars</td>
+                <td>
+                  Sold me "Psycology 101" in pristine condition for a reasonable
+                  price!
+                </td>
+              </tr>
+              <tr>
+                <td>11/13/2022</td>
+                <td>Venn Reddy</td>
+                <td>3.5 Stars</td>
+                <td>
+                  Swapped out his CSCI 1301 class for my CMLT 2400 class but he
+                  was a horrible communicator.
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div>
+            <p className="card-text">Username: {seller}</p>
+          </div>
+          <p className="card-text">Price: ${price}</p> <br />
+          <p className="card-text">Condition: {condition}</p>
+          <a href="#" className="btn btn-primary">
+            View
+          </a>
         </div>
       </div>
     </div>
