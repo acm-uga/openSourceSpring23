ec6cc60c677c8f699f26de558b7e1b7836ec196b
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-04-17 10:56:35 -0400

    Style feedback page and add form link

--- my-app/src/components/FeedbackPage/FeedbackPage.jsx
+++ my-app/src/components/FeedbackPage/FeedbackPage.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 import './FeedbackPage.css';
 import '../MainTheme.css';
 
-// disable eslint for now
+/* UGA Marketplace Feedback Form link */
+const FormLink =
+  'https://docs.google.com/forms/d/e/1FAIpQLSfJkjB6IcYv5ibrBpqzTXmWD3etrhj9NNrRz8mt1vgaxvwQmg/viewform?usp=sf_link';
+
+// Disbable eslint temporarily
 /* eslint-disable */
 function FeedbackPage() {
+  function GotoForm() {
+    window.location.href = FormLink;
+  }
+
   return (
     <div className="main-container-feedback">
       <b>
@@ -17,7 +25,7 @@ function FeedbackPage() {
           faculty member, your feedback is valuable and will be strongly
           considered as the website continues to grow.
         </div>
-        <button type="button" className="form-button">
+        <button onClick={GotoForm} type="button" className="form-button">
           Submit Feedback
         </button>
       </div>
