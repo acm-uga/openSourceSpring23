f85ead0d65fbf619fc227d14f6139c350294adbf
charles4141233 <77245320+charles4141233@users.noreply.github.com>
2023-04-19 16:15:46 -0400

    Implemented helpful toast messages
    Make sure to npm i react-toastify!

--- my-app/src/components/ParkingPassSwap/ParkingPassMain.jsx
+++ my-app/src/components/ParkingPassSwap/ParkingPassMain.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Container, Row, Col, Button, Select, Form } from 'react-bootstrap';
-
 import ParkingPass from './ParkingPass';
 import AddParkingPassModal from './AddParkingPassModal.jsx';
 import './ParkingPassMain.css';
@@ -14,6 +15,7 @@ const ParkingPassMain = () => {
     maxPrice: 999,
     lot: 'Any',
   });
+  const [myPromise, setPromise] = useState();
 
   const getPasses = async () => {
     const response = await fetch(
@@ -54,114 +56,165 @@ const ParkingPassMain = () => {
     setFilterState(originalState);
   };
 
+  // Another way to call the toast message
+  function notifySuccess(text) {
+    toast.success(text, {
+      position: 'top-center',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+  }
+
   useEffect(() => {
-    getPasses();
+    console.log('useEffect running');
+    // showToastMessage(); // reusable toast message
+    setPromise(getPasses());
   }, []);
 
   useEffect(() => {
     setPassesToShow(filterPasses(filterState));
-  }, [passes]);
+    if (passes.length == 0 && myPromise) {
+      // For some reason, the toast message is called twice. I found out that it's b/c of React.Strictmode for the index.js file
+      // Removing the wrapper component fixes the problem
+      toast.promise(
+        myPromise,
+        {
+          pending: 'Retrieving Data',
+          success: 'Data Successfully Displayed',
+          error: 'Error when fetching data. Turn on Backend',
+        },
+        {
+          style: {
+            minWidth: '380px',
+          },
+          success: {
+            duration: 5000,
+          },
+        }
+      );
+    }
+  }, [passes, myPromise]);
 
   const handleAddPass = newPass => {
     setPasses([...passes, newPass]);
+    notifySuccess('The Parking Pass was added successfully!');
   };
 
   return (
-    <Container fluid>
-      <Row>
-        <Col
-          className="side-tab-container"
-          xs={2}
-          style={{ marginTop: '150px' }}>
-          <Button
-            onClick={() => setShowAddModal(true)}
-            style={{
-              width: '100%',
-            }}>
-            Add Parking Pass
-          </Button>
-          <Form.Label
-            style={{
-              marginTop: '10%',
-              color: 'white',
-            }}>
-            Lot
-          </Form.Label>
-          <Form.Select
-            onChange={handleChange}
-            value={filterState.lot}
-            style={{}}
-            name="lot">
-            <option>Any</option>
-            <option>North Deck</option>
-            <option>South Deck</option>
-            <option>East Deck</option>
-            <option>STEM Deck</option>
-          </Form.Select>
-          <Form.Label
-            style={{
-              color: 'white',
-              marginTop: '10%',
-            }}>
-            Max Price ($)
-          </Form.Label>
-          <Form.Control
-            type="number"
-            name="maxPrice"
-            onChange={handleChange}
-            value={filterState.maxPrice}
-          />
-          <Row
-            style={{
-              width: '100%',
-              alignContent: 'center',
-              alignItems: 'center',
-              justifyContent: 'space-evenly',
-              marginTop: '10%',
-            }}>
-            <Col>
-              <Button
-                style={{
-                  marginTop: '10%',
-                  width: '100%',
-                }}
-                onClick={onFilterClick}>
-                Filter
-              </Button>
-            </Col>
-            <Col>
-              <Button
-                style={{
-                  width: '100%',
-                  marginTop: '10%',
-                }}
-                onClick={onClearClick}>
-                Clear
-              </Button>
-            </Col>
-          </Row>
-        </Col>
-        <Col
-          className="parking-pass-list-container"
-          xs={10}
-          style={{ marginTop: '150px' }}>
-          <Row
-            className="parking-pass-list"
-            style={{
-              justifyContent: 'space-around',
-            }}>
-            {passesToShow.map(pass => {
-              return <ParkingPass {...pass} key={pass.id} />;
-            })}
-          </Row>
-        </Col>
-      </Row>
-      <AddParkingPassModal
-        show={showAddModal}
-        onHide={() => setShowAddModal(false)}
-        onSubmit={handleAddPass}
+    <>
+      <Container fluid>
+        <Row>
+          <Col
+            className="side-tab-container"
+            xs={2}
+            style={{ marginTop: '150px' }}>
+            <Button
+              onClick={() => setShowAddModal(true)}
+              style={{
+                width: '100%',
+              }}>
+              Add Parking Pass
+            </Button>
+            <Form.Label
+              style={{
+                marginTop: '10%',
+                color: 'white',
+              }}>
+              Lot
+            </Form.Label>
+            <Form.Select
+              onChange={handleChange}
+              value={filterState.lot}
+              style={{}}
+              name="lot">
+              <option>Any</option>
+              <option>North Deck</option>
+              <option>South Deck</option>
+              <option>East Deck</option>
+              <option>STEM Deck</option>
+            </Form.Select>
+            <Form.Label
+              style={{
+                color: 'white',
+                marginTop: '10%',
+              }}>
+              Max Price ($)
+            </Form.Label>
+            <Form.Control
+              type="number"
+              name="maxPrice"
+              onChange={handleChange}
+              value={filterState.maxPrice}
+            />
+            <Row
+              style={{
+                width: '100%',
+                alignContent: 'center',
+                alignItems: 'center',
+                justifyContent: 'space-evenly',
+                marginTop: '10%',
+              }}>
+              <Col>
+                <Button
+                  style={{
+                    marginTop: '10%',
+                    width: '100%',
+                  }}
+                  onClick={onFilterClick}>
+                  Filter
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  style={{
+                    width: '100%',
+                    marginTop: '10%',
+                  }}
+                  onClick={onClearClick}>
+                  Clear
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+          <Col
+            className="parking-pass-list-container"
+            xs={10}
+            style={{ marginTop: '150px' }}>
+            <Row
+              className="parking-pass-list"
+              style={{
+                justifyContent: 'space-around',
+              }}>
+              {passesToShow.map(pass => {
+                return <ParkingPass {...pass} key={pass.id} />;
+              })}
+            </Row>
+          </Col>
+        </Row>
+        <AddParkingPassModal
+          show={showAddModal}
+          onHide={() => setShowAddModal(false)}
+          onSubmit={handleAddPass}
+        />
+      </Container>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
       />
-    </Container>
+    </>
   );
 };
 
--- my-app/src/components/TicketPage/TicketPage.jsx
+++ my-app/src/components/TicketPage/TicketPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import { Container, Row, Col, Button, Select, Form } from 'react-bootstrap';
 
 import Ticket from './Ticket';
@@ -13,6 +14,7 @@ const TicketPage = () => {
     maxPrice: 999,
     team: 'Any',
   });
+  const [myPromise, setPromise] = useState();
 
   const handleChange = event => {
     const { name, value } = event.target;
@@ -52,118 +54,167 @@ const TicketPage = () => {
       'http://localhost:8080/api/tickets/getTicketsAll'
     );
     const json = await response.json();
+    console.log(json);
     setTickets(json);
     setTicketsToShow(json);
   };
 
+  function notifySuccess(text) {
+    toast.success(text, {
+      position: 'top-center',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+  }
+
   useEffect(() => {
-    getPasses();
+    setPromise(getPasses());
   }, []);
 
   useEffect(() => {
     setTicketsToShow(filterTickets(filterState));
-  }, [tickets]);
+    if (tickets.length == 0 && myPromise) {
+      // For some reason, the toast message is called twice. I found out that it's b/c of React.Strictmode for the index.js file
+      // Removing the wrapper component fixes the problem
+      toast.promise(
+        myPromise,
+        {
+          pending: 'Retrieving Data',
+          success: 'Data Successfully Displayed',
+          error: 'Error when fetching data. Turn on Backend',
+        },
+        {
+          style: {
+            minWidth: '380px',
+          },
+          success: {
+            duration: 5000,
+          },
+        }
+      );
+    }
+  }, [tickets, myPromise]);
 
   const handleAddTicket = newTicket => {
     setTickets([...tickets, newTicket]);
+    notifySuccess('The Ticket has been added successfully!');
   };
 
   return (
-    <Container fluid>
-      <Row>
-        <Col
-          className="side-tab-container"
-          xs={2}
-          style={{ marginTop: '150px' }}>
-          <Button
-            onClick={() => setShowAddModal(true)}
-            style={{
-              width: '100%',
-            }}>
-            Add Ticket
-          </Button>
-          <Form.Label
-            style={{
-              marginTop: '10%',
-              color: 'white',
-            }}>
-            Team
-          </Form.Label>
-          <Form.Select
-            onChange={handleChange}
-            value={filterState.lot}
-            style={{}}
-            name="lot">
-            <option>Any</option>
-            <option>Florida</option>
-            <option>Georgia Tech</option>
-            <option>Alabama</option>
-            <option>Clemson</option>
-          </Form.Select>
-          <Form.Label
-            style={{
-              color: 'white',
-              marginTop: '10%',
-            }}>
-            Max Price ($)
-          </Form.Label>
-          <Form.Control
-            type="number"
-            name="maxPrice"
-            onChange={handleChange}
-            value={filterState.maxPrice}
-          />
-          <Row
-            style={{
-              width: '100%',
-              alignContent: 'center',
-              alignItems: 'center',
-              justifyContent: 'space-evenly',
-              marginTop: '10%',
-            }}>
-            <Col>
-              <Button
-                style={{
-                  marginTop: '10%',
-                  width: '100%',
-                }}
-                onClick={onFilterClick}>
-                Filter
-              </Button>
-            </Col>
-            <Col>
-              <Button
-                style={{
-                  width: '100%',
-                  marginTop: '10%',
-                }}
-                onClick={onClearClick}>
-                Clear
-              </Button>
-            </Col>
-          </Row>
-        </Col>
-        <Col
-          className="parking-pass-list-container"
-          xs={10}
-          style={{ marginTop: '150px' }}>
-          <Row
-            className="parking-pass-list"
-            style={{
-              justifyContent: 'space-around',
-            }}>
-            {ticketsToShow.map(ticket => {
-              return <Ticket {...ticket} key={ticket.id} />;
-            })}
-          </Row>
-        </Col>
-      </Row>
-      <AddTicketModal
-        show={showAddModal}
-        onHide={() => setShowAddModal(false)}
-        onSubmit={handleAddTicket}
+    <>
+      <Container fluid>
+        <Row>
+          <Col
+            className="side-tab-container"
+            xs={2}
+            style={{ marginTop: '150px' }}>
+            <Button
+              onClick={() => setShowAddModal(true)}
+              style={{
+                width: '100%',
+              }}>
+              Add Ticket
+            </Button>
+            <Form.Label
+              style={{
+                marginTop: '10%',
+                color: 'white',
+              }}>
+              Team
+            </Form.Label>
+            <Form.Select
+              onChange={handleChange}
+              value={filterState.lot}
+              style={{}}
+              name="lot">
+              <option>Any</option>
+              <option>Florida</option>
+              <option>Georgia Tech</option>
+              <option>Alabama</option>
+              <option>Clemson</option>
+            </Form.Select>
+            <Form.Label
+              style={{
+                color: 'white',
+                marginTop: '10%',
+              }}>
+              Max Price ($)
+            </Form.Label>
+            <Form.Control
+              type="number"
+              name="maxPrice"
+              onChange={handleChange}
+              value={filterState.maxPrice}
+            />
+            <Row
+              style={{
+                width: '100%',
+                alignContent: 'center',
+                alignItems: 'center',
+                justifyContent: 'space-evenly',
+                marginTop: '10%',
+              }}>
+              <Col>
+                <Button
+                  style={{
+                    marginTop: '10%',
+                    width: '100%',
+                  }}
+                  onClick={onFilterClick}>
+                  Filter
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  style={{
+                    width: '100%',
+                    marginTop: '10%',
+                  }}
+                  onClick={onClearClick}>
+                  Clear
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+          <Col
+            className="parking-pass-list-container"
+            xs={10}
+            style={{ marginTop: '150px' }}>
+            <Row
+              className="parking-pass-list"
+              style={{
+                justifyContent: 'space-around',
+              }}>
+              {ticketsToShow.map(ticket => {
+                return <Ticket {...ticket} key={ticket.id} />;
+              })}
+            </Row>
+          </Col>
+        </Row>
+        <AddTicketModal
+          show={showAddModal}
+          onHide={() => setShowAddModal(false)}
+          onSubmit={handleAddTicket}
+        />
+      </Container>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
       />
-    </Container>
+    </>
   );
 };
 
